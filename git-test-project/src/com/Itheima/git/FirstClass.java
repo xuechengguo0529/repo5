2c7bcd9cbd5bddf17187446dda5fcf0352f6a19b
薛成国 <56473432@qq.com>
2020-05-29 18:07:15 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/Itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.Itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+        System.out.println("git hello");
+    }
+}
